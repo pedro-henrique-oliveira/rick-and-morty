012055ca153a81f55b51d852c6ae95f3e71118fe
pedro-henrique-oliveira <henriqueoliveiradasilvapedro1@gmail.com>
2025-12-01 10:24:57 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,11 @@ function App() {
     <>
       <h1 className={s.title}>Rick and Morty Characters</h1>
       <main>
+        <div style={{display: "flex",flexWrap: "wrap", gap: "20", alignItems: "center", justifyContent: "center"}}>
         <input type="text" value={searchPage} onChange={(e) => setSearchPage(e.target.value)} placeholder='1/42'/>
         <input type="text" value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder='Procure um personagem'/>
-      
+        </div>
+
         <div className={s.wrapCards}>
           {data.map((item, index) => {
             return(
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,11 @@
 import s from './card.module.css'
 
-export const Card = (image, name, species) => {
+export const Card = ({image, name, species}) => {
     return(
-        <>
-            <img src={image} alt={name} />
-            <h2>{name}</h2>
-            <p>{species}</p>
-        </>
+        <div className={s.cardBackground}>
+            <img className={s.cardImage} src={image} alt={name} />
+            <h2 className={s.cardTitle}>{name}</h2>
+            <p className={s.cardText}>{species}</p>
+        </div>
     )
 }
