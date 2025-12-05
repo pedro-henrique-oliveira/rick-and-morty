a14ce21a7b824bcff1937fb5a669723e77f3b809
pedro-henrique-oliveira <henriqueoliveiradasilvapedro1@gmail.com>
2025-12-05 08:13:11 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,43 @@
 import { useState, useEffect } from 'react'
-import s from './App.module.css'
+import s from  './App.module.css'
 import { api } from './api/api'
 import { Card } from './components/card'
-import logo from '../public/logo.png'
 
 function App() {
   const [data, setData] = useState([])
-  const [searchPage, setsearchPage] = useState("")
- 
+  const [searchName, setSearchName] = useState("")
+  const [searchPage, setSearchPage] = useState("")
 
   useEffect(() =>{
-    api.get(`/characters/?page=${searchPage}`).then((response) => {
-      setData(response.data.items)
-      console.log(response.data.items)
+    api.get(`/character/?name=${searchName}&page=${searchPage}`).then((response) => {
+      setData(response.data.results)
     }).catch((error) => {
-      console.error("ERRO AO PROCURAR API", error)
+      console.error("NÃO FOI POSSÍVEL ACESSAR API", error)
     })
-  }, [ searchPage])
+  }, [searchName, searchPage])
 
-
-  return(
+  return (
     <>
+      <h1 className={s.title}>Rick and Morty Characters</h1>
+      <main>
+        <div style={{display: "flex",flexWrap: "wrap", gap: "20", alignItems: "center", justifyContent: "center"}}>
+        <input type="text" value={searchPage} onChange={(e) => setSearchPage(e.target.value)} placeholder='1/42'/>
+        <input type="text" value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder='Procure um personagem'/>
+        </div>
 
-    <img src={logo} className={s.logo}/>
-    <main>
-      <div className={s.input}>
-      <input type="text" value={searchPage} onChange={(e) => setsearchPage(e.target.value)} placeholder={'1/6'}/>
-      </div>
-      <div className={s.wrapCard}>
-        {data.map ((item, index) => {
-          return(
-            <div key={index}>
-              <Card image={item.image} name={item.name} ki={item.ki}/>
-            </div>
-          )
-        }
+        <div className={s.wrapCards}>
+          {data.map((item, index) => {
+            return(
+              <div key={index}>
+                  <Card image={item.image} name={item.name} species={item.species}/>
+              </div>
+            )
+          })}
 
-        )}
-
-      </div>
-    </main>
+        </div>
+      </main>
     </>
   )
-  }
-
+}
 
 export default App
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,12 +1,11 @@
 import s from './card.module.css'
 
-export const Card = ({image, name, ki}) => {
+export const Card = ({image, name, species}) => {
     return(
         <div className={s.cardBackground}>
             <img className={s.cardImage} src={image} alt={name} />
-            <h2 className={s.cardName}>{name}</h2>
-            <p className={s.cardki}>{ki}</p>
-
+            <h2 className={s.cardTitle}>{name}</h2>
+            <p className={s.cardText}>{species}</p>
         </div>
     )
 }
